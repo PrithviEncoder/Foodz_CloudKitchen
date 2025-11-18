9e464902b4a895bc520c2b792874818eca6ff445
PrithviEncoder <yourencoder07@gmail.com>
2025-11-19 02:22:27 +0530

    add resend mail service and remove sendgrid

--- backend/src/mail/mail.config.js
+++ backend/src/mail/mail.config.js
@@ -1,13 +1,22 @@
-import sgMail from '@sendgrid/mail';
-import dotenv from 'dotenv'
+import { Resend } from "resend";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+export const resend = new Resend(process.env.RESEND_API_KEY);
 
-export { sgMail };
 
-export const sender = {
-  email: "prithvichawda1@gmail.com",
-  name: "Cloud Kitchen",
-};
+
+// import sgMail from '@sendgrid/mail';
+// import dotenv from 'dotenv'
+
+// dotenv.config();
+
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// export { sgMail };
+
+// export const sender = {
+//   email: "prithvichawda1@gmail.com",
+//   name: "Cloud Kitchen",
+// };
--- backend/src/mail/mail.methods.js
+++ backend/src/mail/mail.methods.js
@@ -1,20 +1,20 @@
 
-import { sgMail, sender } from './mail.config.js'
+import { resend } from './mail.config.js'
 import { CLOUD_KITCHEN_PASSWORD_RESET_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from './mail.template.js';
 
 export const sendVerificationMail = async (email, verificationToken) => {
   
     try {
-        const response = await sgMail.send({
-            from: sender,
-            to: email,
+        const response = await resend.emails.send({
+            from: 'Cloud Kitchen <Prithvi@cloudkitchenfoodz.in>',
+            to: [email],
             subject: "Verify Your Email",
             html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
         
         })
 
     // Proper logging for SendGrid response
-    console.log(`Verification code email successfully sent to ${email}: ${response[0].statusCode}`);
+     console.log("Email sent successfully:", response.id);  
         
     } catch (error) {
         console.error(`Error sending Verification : ${error}`);   
@@ -24,14 +24,13 @@ export const sendVerificationMail = async (email, verificationToken) => {
 export const sendPasswordResetMail = async (email, resetURL) => {
     
     try {
-        const response = await sgMail.send({
-            from: sender,
-            to: email,
+        const response = await resend.emails.send({
+            from: 'Cloud Kitchen <Prithvi@cloudkitchenfoodz.in>',
+            to: [email],
             subject: "Reset Your Password",
             html:CLOUD_KITCHEN_PASSWORD_RESET_TEMPLATE.replace("{resetURL}", resetURL),
-            category: "Password reset mail"
         })
-        console.log(`Password Reset Mail Successfully Sent to ${email}: ${response[0].statusCode}`);
+        console.log("Email sent successfully:", response.id);
 
     } catch (error) {
         console.error("There is some error is sending password reset mail : ", error);
@@ -42,14 +41,13 @@ export const sendPasswordResetMail = async (email, resetURL) => {
 export const sendPasswordResetSuccessMail = async (email) => {
     
     try {
-        const response = await sgMail.send({
-            from: sender,
-            to: email,
+        const response = await resend.emails.send({
+            from: 'Cloud Kitchen <Prithvi@cloudkitchenfoodz.in>',
+            to: [email],
             subject: "Password Reset Successfull",
             html: PASSWORD_RESET_SUCCESS_TEMPLATE,
-            category: "Reset Password Mail"
         })
-        console.log(`Password Reset Success email sent to ${email}: ${response[0].statusCode}`)
+        console.log("Email sent successfully:", response.id)
 
     } catch (error) {
         console.error("There is some error is sending Successfull password reset Mail: ", error)
