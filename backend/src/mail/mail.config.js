6d75e7c7bf52f37cd138464ea91913692229a2c7
PrithviEncoder <yourencoder07@gmail.com>
2025-11-19 02:27:17 +0530

    test

--- backend/src/mail/mail.config.js
+++ backend/src/mail/mail.config.js
@@ -6,17 +6,3 @@ dotenv.config();
 export const resend = new Resend(process.env.RESEND_API_KEY);
 
 
-
-// import sgMail from '@sendgrid/mail';
-// import dotenv from 'dotenv'
-
-// dotenv.config();
-
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// export { sgMail };
-
-// export const sender = {
-//   email: "prithvichawda1@gmail.com",
-//   name: "Cloud Kitchen",
-// };
